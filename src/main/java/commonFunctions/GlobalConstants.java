6fa04c67218bbfebbd4607ef361d1b074148e499
phammDesktop <phamminhquan2020@gmail.com>
2020-08-09 00:20:16 +0700

    update directory

--- src/main/java/commonFunctions/GlobalConstants.java
+++ src/main/java/commonFunctions/GlobalConstants.java
@@ -4,8 +4,8 @@ public class GlobalConstants {
 
     public static final long LONG_TIME_OUT = 30;
     public static final long SHORT_TIME_OUT = 10;
-    public static final String UPLOAD_FOLDER = System.getProperty("user.dir") + "\\upLoadFiles";
-    public static final String EXTENSION_FOLDER = System.getProperty("user.dir") + "\\browserExtensions";
+    public static final String UPLOAD_FOLDER = System.getProperty("user.dir") + "/upLoadFiles";
+    public static final String EXTENSION_FOLDER = System.getProperty("user.dir") + "/browserExtensions";
 
 
     public static final String USER_URL = "https://demo.nopcommerce.com/";
