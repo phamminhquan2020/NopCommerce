3ee8d92955d975e9f4f6f8a485527df0de888028
phammDesktop <phamminhquan2020@gmail.com>
2020-08-09 00:44:59 +0700

    update directory

--- src/main/java/reportConfig/ReportNGListener.java
+++ src/main/java/reportConfig/ReportNGListener.java
@@ -69,7 +69,7 @@ public class ReportNGListener extends AbstractTest implements ITestListener {
             Calendar calendar = Calendar.getInstance();
             SimpleDateFormat formater = new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
             File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-            String screenPath = System.getProperty("user.dir") + "\\ReportNGScreenShots\\" + screenshotName + "_" + formater.format(calendar.getTime()) + ".png";
+            String screenPath = System.getProperty("user.dir") + "/ReportNGScreenShots/" + screenshotName + "_" + formater.format(calendar.getTime()) + ".png";
             FileUtils.copyFile(source, new File(screenPath));
             return screenPath;
         } catch (IOException e) {
